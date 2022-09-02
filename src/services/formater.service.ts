a4eef45fa8a9ec2599aa403012fbb6b40de0b626
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2022-09-02 12:48:07 +0300

    fix: Fix formating config

--- src/services/formater.service.ts
+++ src/services/formater.service.ts
@@ -1,9 +1,9 @@
 export function formatPrice(price: number): string {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(price);
+    return new Intl.NumberFormat('ru', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(price);
 }
 
 export function formatMileage(mileage: number): string {
-    return new Intl.NumberFormat().format(mileage) + ' км';
+    return new Intl.NumberFormat('ru').format(mileage) + ' км';
 }
 
 export function formatYear(year: number): string {
